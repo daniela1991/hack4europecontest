db40d58963cd93150f8f6e9ce7ceb6bdfa8a72d4
daniela1991 <marc.daniela91@gmail.com>
2015-06-09 13:38:37 -0700

    Create GetCartResponse.cs

---
+++ GetCartResponse.cs
@@ -0,0 +1,56 @@
+using System;
+using Cdiscount.OpenApi.ProxyClient.Contract.Common;
+
+namespace Cdiscount.OpenApi.ProxyClient.Contract.GetCart
+{
+    /// <summary>
+    /// Cart content message
+    /// </summary>
+    public class GetCartResponse : BaseResponseMessage
+    {
+        /// <summary>
+        /// Cart identifier
+        /// </summary>
+        public Guid CartGuid { get; set; }
+
+        /// <summary>
+        /// Url to checkout order
+        /// </summary>
+        public string CheckoutUrl { get; set; }
+
+        /// <summary>
+        /// Cart creation date
+        /// </summary>
+        public DateTime CreationDate { get; set; }
+
+        /// <summary>
+        /// Cart content items
+        /// </summary>
+        public CartItemList Items { get; set; }
+
+        /// <summary>
+        /// Product quantity
+        /// </summary>
+        public int ProductCount { get; set; }
+
+        /// <summary>
+        /// Cart total price
+        /// </summary>
+        public decimal TotalPrice { get; set; }
+
+        /// <summary>
+        /// Item quantity
+        /// </summary>
+        public int TotalQuantity { get; set; }
+
+        /// <summary>
+        /// Cart update date
+        /// </summary>
+        public DateTime UpdateDate { get; set; }
+
+        /// <summary>
+        /// Error type
+        /// </summary>
+        public string ErrorType { get; set; }
+    }
+}
