b4142736845641d6deca561aec456ff6ce3e40cc
daniela1991 <marc.daniela91@gmail.com>
2015-06-09 13:31:14 -0700

    Create ProductPriceSaving.cs

---
+++ ProductPriceSaving.cs
@@ -0,0 +1,18 @@
+namespace Cdiscount.OpenApi.ProxyClient.Contract.Common
+{
+    /// <summary>
+    /// Product price saving details
+    /// </summary>
+    public class ProductPriceSaving
+    {
+        /// <summary>
+        /// Saving details (amount or ratio)
+        /// </summary>
+        public decimal Type { get; set; }
+
+        /// <summary>
+        /// Saving value
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
