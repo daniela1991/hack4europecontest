b33ba3b0070a808eb4fbb8f9ea75eea32c024c9f
daniela1991 <marc.daniela91@gmail.com>
2015-06-09 13:31:49 -0700

    Create ProductSeller.cs

---
+++ ProductSeller.cs
@@ -0,0 +1,18 @@
+namespace Cdiscount.OpenApi.ProxyClient.Contract.Common
+{
+    /// <summary>
+    /// Product seller informations
+    /// </summary>
+    public class ProductSeller
+    {
+        /// <summary>
+        /// Seller identifier
+        /// </summary>
+        public string Id { get; set; }
+        
+        /// <summary>
+        /// Product name
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
