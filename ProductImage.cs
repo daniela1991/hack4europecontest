2588f48303ecb1dfbbf22b34cde2719c2f898370
daniela1991 <marc.daniela91@gmail.com>
2015-06-09 13:26:33 -0700

    Create ProductImage.cs

---
+++ ProductImage.cs
@@ -0,0 +1,17 @@
+namespace Cdiscount.OpenApi.ProxyClient.Contract.Common
+{
+    /// <summary>
+    /// Product image entity
+    /// </summary>
+    public class ProductImage
+    {
+        /// <summary>
+        /// Product image URL
+        /// </summary>
+        public string ImageUrl { get; set; }
+        /// <summary>
+        /// Product image ThumbnailUrl
+        /// </summary>
+        public string ThumbnailUrl { get; set; }
+    }
+}
